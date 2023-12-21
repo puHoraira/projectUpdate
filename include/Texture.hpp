939dc72b8c50f3599e2497f6b38a37c8a16aca78
puHoraira <126349255+puHoraira@users.noreply.github.com>
2023-12-21 22:44:54 +0600

    Add files via upload

---
+++ include/Texture.hpp
@@ -0,0 +1,51 @@
+//#ifndef GAME_STATE_HPP
+//#define GAME_STATE_HPP
+//#include "Graphics.hpp"
+#include "common.hpp"
+//#endif
+
+#ifndef SDL_INCLUDES
+#define SDL_INCLUDES
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL.h>
+#endif
+
+
+
+
+#ifndef TEXTURE_HPP
+#define TEXTURE_HPP
+
+#include <string>
+class Texture {
+    public:
+        //inicia as variaveis
+        Texture();
+
+        //desaloca memoria
+        ~Texture();
+
+        //carrega imagens de um arquivo
+        bool loadFromFile( std::string path);
+
+        //Desaloca textura
+        void free();
+
+        //Renderiza textura em algum ponto
+        void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE );
+
+        //dimensoes da imagem
+        int getWidth();
+        int getHeight();
+
+    private:
+        //a textura
+        SDL_Texture* mTexture;
+
+        //dimensoes da imagem
+        int mWidth;
+        int mHeight;
+
+};
+
+#endif
